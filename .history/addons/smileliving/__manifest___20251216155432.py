31a922c47ebae3a0e8c829da562137ab74e0c8b6
unknown <lamkiet0902741206@gmail.com>
2025-12-16 17:01:50 +0700

    Completely basic form 16/12/25

---
+++ .history/addons/smileliving/__manifest___20251216155432.py
@@ -0,0 +1,25 @@
+{
+    'name': "SmileLiving",
+    'version': '1.0',
+    'depends': ['base', 'mail', 'web', 'website', 'website_sale', 'crm'],
+    'assets': {
+        'web.assets_frontend': [
+            'smileliving/static/src/interactions/area_range.js',
+            'smileliving/static/src/css/multirange_counters_fix.css',
+        ],
+    },
+    'data': [
+        'security/ir.model.access.csv',
+        'views/house_property_views.xml',
+        'views/invoice_property_views.xml',
+        'views/property_type_views.xml',
+        'views/website_sale_interest_inherit.xml',
+        'views/website_sale_wishlist_interest_inherit.xml',
+        'views/smileliving_template_views.xml',
+        'views/product_item_inherit.xml',
+        'views/wishlist_minimal_tile_inherit.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
